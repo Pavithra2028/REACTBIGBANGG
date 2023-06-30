d9189e04d8182e7dc4c67d16b2ae5cd60dbd04c7
Pavithra2028 <pavithra.kanini@gmail.com>
2023-06-30 12:25:14 +0530

    Add project files.

---
+++ REACTBIGBANG/Models/Admin.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace REACTBIGBANG.Models
+{
+    public class Admin
+    {
+        [Key]
+
+        public int Admin_id { get; set; }
+
+        public string Admin_name { get; set; }
+
+        public string Admin_password { get; set; }
+
+    }
+}
---
+++ REACTBIGBANG/Models/Doctor.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace REACTBIGBANG.Models
+{
+    public class Doctor
+    {
+        [Key]
+
+       public int Doctor_id { get; set; }
+
+       public string Doctor_name { get; set; }
+
+       public string Specialization { get; set; }
+
+       public string Doctor_gender { get; set; }
+
+       public int Doctor_experience { get; set; }
+
+       public string Doctor_password { get; set; }
+
+       public ICollection<Patient> Patients { get; set; }
+
+    }
+}
